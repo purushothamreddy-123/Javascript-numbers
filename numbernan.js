3efbf9de50776fd0d5ed372d337e012148686e21
Purushothamreddy <purushothamreddydevana@gmail.com>
2022-08-16 18:26:33 +0530

    Javascript numbers

---
+++ numbernan.js
@@ -0,0 +1,5 @@
+var a = isNaN(9);
+console.log(a); 
+
+var b = isNaN(4 - 'hello');
+console.log(b);
---
+++ operatorwithnumbers.js
@@ -0,0 +1,8 @@
+var a = '4' - 2;
+console.log(a); 
+
+var b = '4' / 2;
+console.log(a); 
+
+var c = '4' * 2;
+console.log(a); 
